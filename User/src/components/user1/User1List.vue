5b3a98d43c7e8ff65e992a1fc9d20f6e375f04d0
chhak0503 <chhak0503@gmail.com>
2023-04-18 09:51:12 +0900

    2023/04/18 수업예제

---
+++ User/src/components/user1/User1List.vue
@@ -0,0 +1,25 @@
+<template>
+  <h4>User1 목록</h4>
+
+  <table border="1">
+    <tr>
+      <th>아이디</th>
+      <th>이름</th>
+      <th>휴대폰</th>
+      <th>나이</th>
+      <th>관리</th>
+    </tr>
+    <tr>
+      <td>a101</td>
+      <td>홍길동</td>
+      <td>010-1213-1212</td>
+      <td>15</td>
+      <td>
+        <a href="#">수정</a>
+        <a href="#">삭제</a>
+      </td>
+    </tr>
+  </table>
+</template>
+<script setup></script>
+<style scoped></style>
